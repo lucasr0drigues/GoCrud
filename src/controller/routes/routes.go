2bf9f383ec4962e9fd8bee8320bda12aa8b20cfb
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-11-17 18:07:30 -0300

    add handler to routes initializer

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucasr0drigues/GoCrud/src/controller"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userId")
-	r.GET("/getUserByEmail/:userEmail")
-	r.POST("/createUser")
-	r.PUT("/updateUser/:userId")
-	r.DELETE("/deleteUser/:userId")
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
