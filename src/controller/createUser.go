86511397c613011fdee485aa02e80ade9aad09fa
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-11-28 23:30:27 -0300

    add user request and response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucasr0drigues/GoCrud/src/configuration/rest_err"
+)
 
 func CreateUser(c *gin.Context) {
-
+	err := rest_err.NewBadRequestError("Você chamou a rtoa de forma errada")
+	c.JSON(err.Code, err)
 }
