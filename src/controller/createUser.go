9b8cce68d1013e3f1aa55901d8d21d585809c6b4
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-11-28 23:41:29 -0300

    refactor CreateUser

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lucasr0drigues/GoCrud/src/configuration/rest_err"
+	"github.com/lucasr0drigues/GoCrud/src/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rtoa de forma errada")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorret fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
