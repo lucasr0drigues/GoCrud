d7103af0f3123d23b31b1ff8b52c34771fab412a
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-12-03 23:07:17 -0300

    create validate user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,23 +2,35 @@ package controller
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lucasr0drigues/GoCrud/src/configuration/rest_err"
+	"github.com/lucasr0drigues/GoCrud/src/configuration/validation"
 	"github.com/lucasr0drigues/GoCrud/src/model/request"
+	"github.com/lucasr0drigues/GoCrud/src/model/response"
 )
 
 func CreateUser(c *gin.Context) {
 
+	log.Println("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorret fields, error=%s\n", err.Error()))
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
 	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
